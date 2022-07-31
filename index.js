ddbd67810ffd00f6f78cf59db8c84da8be5a6727
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-31 13:51:42 +0100

    completed html / css implementation
    javascript on going

--- index.js
+++ index.js
@@ -3,3 +3,32 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const input = 20;
+
+function meterToFeet(input) {
+  return result = Math.round((input * 3.281) * 1000) / 1000
+}
+
+function feetToMeter(input) {
+  return result = Math.round((input / 3.281) * 1000) / 1000
+  
+}
+
+function literTogallon(input) {
+  return result = input * 0.264
+}
+
+function gallonToLiter(input) {
+  return result = input / 0.264
+}
+
+function kilogramToPound(input) {
+  return result = input * 2.204
+}
+
+function poundToKilogram(input) {
+  return result = input / 2.204
+}
+
+console.log(feetToMeter(input))
