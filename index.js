03f6fef06536a9fcc6e371a362934165dd13b957
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-31 17:05:34 +0100

    loop nodelist of output value to print out each  value

--- index.js
+++ index.js
@@ -4,7 +4,22 @@
 1 kilogram = 2.204 pound
 */
 
-const input = 20;
+const input = document.querySelector('#convert-input');
+const inputValue = document.querySelectorAll('.inputValue');
+const convertBtn = document.querySelector('.convert');
+console.log(input.value)
+
+convertBtn.addEventListener('click', ()=> {
+  outputInputValue()
+  console.log(feetToMeter(input))
+  console.log(input)
+})
+
+function outputInputValue() {
+  for(child of inputValue) {
+    child.textContent = 12;
+  }
+}
 
 function meterToFeet(input) {
   return result = Math.round((input * 3.281) * 1000) / 1000
@@ -31,4 +46,3 @@ function poundToKilogram(input) {
   return result = Math.round((input / 2.204) * 1000) / 1000
 }
 
-console.log(feetToMeter(input))
